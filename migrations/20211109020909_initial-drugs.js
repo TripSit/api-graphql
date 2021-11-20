e2a92bf3d9f16e3bfb2276180aa67c69ff0cd04e
SevenCatsHere <sevencats@tripsit.me>
2021-11-20 09:48:29 -0500

    asdf

--- migrations/20211109020909_initial-drugs.js
+++ migrations/20211109020909_initial-drugs.js
@@ -1,6 +1,135 @@
 'use strict';
 
 exports.up = async function up(knex) {
+  function drugsTable(table) {
+    table.text('summary');
+    table.text('psychonautwikiSlug');
+    table.text('erowidExperiencesUrl');
+
+    table
+      .boolean('deleted')
+      .notNullable()
+      .defaultTo(false);
+
+    table
+      .uuid('createdBy')
+      .notNullable()
+      .references('id')
+      .inTable('users');
+
+    table
+      .timestamp('createdAt')
+      .notNullable()
+      .defaultTo(knex.fn.now());
+  }
+
+  function drugNamesTable(table) {
+    table
+      .uuid('drugId')
+      .notNullable()
+      .references('id')
+      .inTable('drugs')
+      .onDelete('CASCADE');
+
+    table.text('name').notNullable();
+
+    table
+      .enum('type', [
+        'COMMON',
+        'SUBSTITUTIVE',
+        'SYSTEMATIC',
+      ], {
+        useNative: true,
+        enumName: 'drug_name_type',
+      })
+      .notNullable()
+      .defaultTo('COMMON');
+
+    table
+      .boolean('primary')
+      .notNullable()
+      .defaultTo(false);
+
+    table
+      .timestamp('createdAt')
+      .notNullable()
+      .defaultTo(knex.fn.now());
+  }
+
+  function drugVariantsTable(table) {
+    table
+      .uuid('drugId')
+      .notNullable()
+      .references('id')
+      .inTable('drugs')
+      .onDelete('CASCADE');
+
+    table.text('name');
+
+    table
+      .boolean('isDefault')
+      .notNullable()
+      .defaultTo(false);
+
+    table
+      .timestamp('createdAt')
+      .notNullable()
+      .defaultTo(knex.fn.now());
+  }
+
+  function drugRoasTable(table) {
+    table
+      .uuid('drugVariantId')
+      .notNullable()
+      .references('id')
+      .inTable('drugVariants')
+      .onDelete('CASCADE');
+
+    table
+      .enum('route', [
+        'ORAL',
+        'INSUFFLATED',
+        'INHALED',
+        'TOPICAL',
+        'SUBLINGUAL',
+        'BUCCAL',
+        'RECTAL',
+        'INTRAMUSCULAR',
+        'INTRAVENOUS',
+        'SUBCUTANIOUS',
+        'TRANSDERMAL',
+      ], {
+        useNative: true,
+        enumName: 'drug_roa',
+      })
+      .notNullable();
+
+    table.float('doseThreshold');
+    table.float('doseLight');
+    table.float('doseCommon');
+    table.float('doseStrong');
+    table.float('doseHeavy');
+    table.text('doseWarning');
+
+    table.float('durationTotalMin');
+    table.float('durationTotalMax');
+    table.float('durationOnsetMin');
+    table.float('durationOnsetMax');
+    table.float('durationComeupMin');
+    table.float('durationComeupMax');
+    table.float('durationPeakMin');
+    table.float('durationPeakMax');
+    table.float('durationOffsetMin');
+    table.float('durationOffsetMax');
+    table.float('durationAfterEffectsMin');
+    table.float('durationAfterEffectsMax');
+
+    table
+      .timestamp('createdAt')
+      .notNullable()
+      .defaultTo(knex.fn.now());
+  }
+
   await knex.schema
     .createTable('drugs', table => {
       table
@@ -8,15 +137,15 @@ exports.up = async function up(knex) {
         .notNullable()
         .defaultTo(knex.raw('uuid_generate_v4()'))
         .primary();
-
-      table.text('summary');
-      table.text('psychonautwikiSlug');
-      table.text('erowidExperiencesUrl');
-
+      drugsTable(table);
+    })
+    .createTable('drugsHistory', table => {
       table
-        .timestamp('createdAt')
+        .uuid('id')
         .notNullable()
-        .defaultTo(knex.fn.now());
+        .references('id')
+        .inTable('drugs');
+      drugsTable(table);
     })
     .createTable('drugNames', table => {
       table
@@ -24,37 +153,15 @@ exports.up = async function up(knex) {
         .notNullable()
         .defaultTo(knex.raw('uuid_generate_v4()'))
         .primary();
-
+      drugNamesTable(table);
+    })
+    .createTable('drugNamesHistory', table => {
       table
-        .uuid('drugId')
+        .uuid('id')
         .notNullable()
         .references('id')
-        .inTable('drugs')
-        .onDelete('CASCADE');
-
-      table.text('name').notNullable();
-
-      table
-        .enum('type', [
-          'COMMON',
-          'SUBSTITUTIVE',
-          'SYSTEMATIC',
-        ], {
-          useNative: true,
-          enumName: 'drug_name_type',
-        })
-        .notNullable()
-        .defaultTo('COMMON');
-
-      table
-        .boolean('primary')
-        .notNullable()
-        .defaultTo(false);
-
-      table
-        .timestamp('createdAt')
-        .notNullable()
-        .defaultTo(knex.fn.now());
+        .inTable('drugNames');
+      drugNamesTable(table);
     })
     .createTable('drugVariants', table => {
       table
@@ -62,25 +169,15 @@ exports.up = async function up(knex) {
         .notNullable()
         .defaultTo(knex.raw('uuid_generate_v4()'))
         .primary();
-
+      drugVariantsTable(table);
+    })
+    .createTable('drugVariantsHistory', table => {
       table
-        .uuid('drugId')
+        .uuid('id')
         .notNullable()
         .references('id')
-        .inTable('drugs')
-        .onDelete('CASCADE');
-
-      table.text('name');
-
-      table
-        .boolean('isDefault')
-        .notNullable()
-        .defaultTo(false);
-
-      table
-        .timestamp('createdAt')
-        .notNullable()
-        .defaultTo(knex.fn.now());
+        .inTable('drugVariants');
+      drugVariantsTable(table);
     })
     .createTable('drugRoas', table => {
       table
@@ -88,65 +185,27 @@ exports.up = async function up(knex) {
         .notNullable()
         .defaultTo(knex.raw('uuid_generate_v4()'))
         .primary();
-
+      drugRoasTable(table);
+    })
+    .createTable('drugRoasHistory', table => {
       table
-        .uuid('drugVariantId')
+        .uuid('id')
         .notNullable()
         .references('id')
-        .inTable('drugVariants')
-        .onDelete('CASCADE');
-
-      table
-        .enum('route', [
-          'ORAL',
-          'INSUFFLATED',
-          'INHALED',
-          'TOPICAL',
-          'SUBLINGUAL',
-          'BUCCAL',
-          'RECTAL',
-          'INTRAMUSCULAR',
-          'INTRAVENOUS',
-          'SUBCUTANIOUS',
-          'TRANSDERMAL',
-        ], {
-          useNative: true,
-          enumName: 'drug_roa',
-        })
-        .notNullable();
-
-      table.float('doseThreshold');
-      table.float('doseLight');
-      table.float('doseCommon');
-      table.float('doseStrong');
-      table.float('doseHeavy');
-      table.text('doseWarning');
-
-      table.float('durationTotalMin');
-      table.float('durationTotalMax');
-      table.float('durationOnsetMin');
-      table.float('durationOnsetMax');
-      table.float('durationComeupMin');
-      table.float('durationComeupMax');
-      table.float('durationPeakMin');
-      table.float('durationPeakMax');
-      table.float('durationOffsetMin');
-      table.float('durationOffsetMax');
-      table.float('durationAfterEffectsMin');
-      table.float('durationAfterEffectsMax');
-
-      table
-        .timestamp('createdAt')
-        .notNullable()
-        .defaultTo(knex.fn.now());
+        .inTable('drugRoas');
+      drugRoasTable(table);
     });
 };
 
 exports.down = async function down(knex) {
   await knex.schema
+    .dropTableIfExists('drugRoasHistory')
     .dropTableIfExists('drugRoas')
+    .dropTableIfExists('drugVariantsHistory')
     .dropTableIfExists('drugVariants')
+    .dropTableIfExists('drugNamesHistory')
     .dropTableIfExists('drugNames')
+    .dropTableIfExists('drugsHistory')
     .dropTableIfExists('drugs');
 
   await knex.raw('DROP TYPE IF EXISTS "drug_name_type"');
--- server/schema/drug.js
+++ server/schema/drug.js
@@ -34,6 +34,7 @@ exports.typeDefs = gql`
     summary: String
     psychonautwikiSlug: String
     errowidExperiencesUrl: String
+    createdBy: User!
     createdAt: DateTime!
   }
 
@@ -49,7 +50,7 @@ exports.typeDefs = gql`
 exports.resolvers = {
   Query: {
     async drugs(root, params, { dataSources }) {
-      return dataSources.db.knex('drugs');
+      return dataSources.db.knex('drugs').where('deleted', false);
     },
   },
 
@@ -81,14 +82,17 @@ exports.resolvers = {
     async updateDrug(root, { drugId, updates }, { dataSources }) {
       return dataSources.db.knex.transacting(async trx => {
         await trx('drugs').where('id', drugId).updates(updates);
-        return trx('drugs').where('id', drugId).first();
+        return trx('drugs')
+          .where('id', drugId)
+          .where('deleted', false)
+          .first();
       });
     },
 
     async removeDrug(root, { drugId }, { dataSources }) {
       return dataSources.db.knex('drugs')
         .where('id', drugId)
-        .del();
+        .updates('deleted', true);
     },
   },
 
@@ -111,6 +115,10 @@ exports.resolvers = {
 
     async variants(drug, params, { dataSources }) {
       return dataSources.db.knex('drugVariants').where('drugId', drug.id);
+    },
+
+    async createdBy(drug, params, { dataSources }) {
+      return dataSources.db.knex('users').where('id', drug.createdBy).first();
     },
   },
 };
